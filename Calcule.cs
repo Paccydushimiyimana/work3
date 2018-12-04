38c04aab23d05c7e618dfe2d0f8eda8a4a3bf142
Paccymuhoza <dushimeshaffy@yahoo.com>
2018-12-05 00:06:04 +0200

    simple calculator

---
+++ Calcule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace New
+{
+   public class Calcule
+    {
+      public int Sum(int x,int y)
+       { 
+        int ans;
+        ans=x+y;
+        return ans; 
+       } 
+      public int Diff(int x,int y)
+       {
+        int ans;
+        ans=x-y;
+        return ans;
+       } 
+      public int Prod(int x,int y)
+       {
+        int ans;
+        ans=x*y;
+        return ans;
+       } 
+      public double Quot(double x,double y)
+       {
+        double ans;
+        ans=x/y;
+        return ans;
+       } 
+      public double Avg(double x,double y)
+       {
+        double ans;
+        ans=((x=y)/2);
+        return ans;
+       } 
+    }   
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace New
+{
+  class Program 
+    {
+      static void Main(string[] args)
+       {
+          var obj=new Calcule();
+          int a,b;
+          char s;
+          Console.WriteLine("start your operation by pressing enter after each entry");
+          Console.WriteLine("use + to add;- to substact;* to multiply;/ to divide;# to average");
+          Console.Write(":");
+          Console.Write("     "); Console.Write("     ");
+          a=int.Parse(Console.ReadLine());
+          Console.Write(" "); 
+          s=char.Parse(Console.ReadLine());
+          Console.Write("      ");
+          b=int.Parse(Console.ReadLine());
+          Console.Write("--------");
+          Console.WriteLine(" ");
+          Console.Write(" = ");
+          Console.Write("  ");
+          if(s=='+')
+          Console.WriteLine(obj.Sum(a,b));
+          else if(s=='-')
+          Console.WriteLine(obj.Diff(a,b));
+          else if(s=='*')
+          Console.WriteLine(obj.Prod(a,b));
+          else if(s=='/')
+          Console.WriteLine(obj.Quot(a,b));
+          else if(s=='#')
+          Console.WriteLine(obj.Avg(a,b));
+          else
+          Console.Write("invalid input(s)");
+          Console.WriteLine();
+          Console.Write("type dotnet run for another operation.");
+        }
+    }
+}    
